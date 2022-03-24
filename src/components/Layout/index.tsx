918085fa9f5a1c2e3a668a92f2247b8c199058cc
Chilli Chelli <ramin@sushi.com>
2022-03-24 16:55:18 +0100

    fix: footer overlaying submit button

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,7 +5,10 @@ export default function Layout({ children }) {
   return (
     <div className="z-0 flex flex-col items-center w-full h-screen">
       <Header />
-      <main className="mt-[100px] flex flex-col items-center justify-start flex-grow w-full h-full">
+      <main
+        className="mt-[100px] flex flex-col items-center justify-start flex-grow w-full h-full"
+        style={{ height: "max-content" }}
+      >
         {children}
       </main>
       <Footer />
